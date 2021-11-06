0eb7cf13aa0040749e58d47707d300727b4fdcbb
Rui Rodrigues <ruirodrigues95@outlook.pt>
2021-11-06 23:26:54 +0000

    test

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -2,19 +2,15 @@ const Header = props => <h1>{props.courseName}</h1>
 
 const Content = ({ parts }) => (
 	<>
-    {parts.map(part =>
-      <Part key={part.id} name={part.name} numExercises={part.exercises} />
-    )}
+		{parts.map(part => (
+			<Part key={part.id} name={part.name} numExercises={part.exercises} />
+		))}
 	</>
 )
 
 const Total = ({ parts }) => {
-  const sum = parts.reduce((sum, part) => sum + part.exercises, 0)
-  return (
-    <p>
-      Number of exercises {sum}
-    </p>
-  )
+	const sum = parts.reduce((sum, part) => sum + part.exercises, 0)
+	return <p>Number of exercises {sum}</p>
 }
 
 const Part = props => (
@@ -24,14 +20,13 @@ const Part = props => (
 )
 
 const Course = ({ course }) => {
-  return (
-    <div>
-      <Header courseName={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-
+	return (
+		<div>
+			<Header courseName={course.name} />
+			<Content parts={course.parts} />
+			<Total parts={course.parts} />
+		</div>
+	)
 }
 
 export default Course
